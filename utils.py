56d0b6ad9212a5bf479c8e2613d6bd03989e2c6f
Diogo <58195309+diogopelaes@users.noreply.github.com>
2022-05-20 07:35:26 -0300

    Atualizar

--- utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime as dt
-from datetime import date
-
-def now(fmt = "%H:%M:%S"):
-    return dt.now().strftime(fmt)
-
-def today(fmt = "%d/%m/%Y"):
-    return date.today().strftime(fmt)
-    
